906db8a7473984108cafd6cffca57aea77eff676
William Boxhall <williamboxhall@gmail.com>
2019-01-02 19:01:31 +1100

    day 1 improve lambda readability

--- src/day1.py
+++ src/day1.py
@@ -3,6 +3,6 @@ import sys
 
 with open(sys.argv[1]) as input:
 	all_lines = input.readlines()
-	all_numbers = map(lambda x: int(x), all_lines)
+	all_numbers = map(int, all_lines)
 	total = sum(all_numbers)
 	print total
